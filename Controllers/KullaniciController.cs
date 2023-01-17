13966a029effed43edfd230da8ac906043de6706
Fatih Demir <90066232+mgwfwt@users.noreply.github.com>
2023-01-17 19:12:02 +0300

    Add files via upload

---
+++ Controllers/KullaniciController.cs
@@ -0,0 +1,36 @@
+﻿using batuhanbilet.business.Abstract;
+using BatuhanBilet.webui.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatuhanBilet.webui.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        private IBiletService _biletService;
+        public KullaniciController(IBiletService biletService)
+        {
+            this._biletService = biletService;
+        }
+        public IActionResult AdminList()
+        {
+            return View(new BiletGuzergah()
+            {
+                Bilets = _biletService.GetAll()
+            });
+        }
+
+        public IActionResult IptalBilet(int biletId)
+        {
+            var bilet = _biletService.GetById(biletId);
+            if (bilet!=null)
+            {
+                _biletService.Delete(bilet);
+            }
+            return RedirectToAction("AdminList");
+        }
+    }
+}
---
+++ Models/BiletGuzergah.cs
@@ -0,0 +1,20 @@
+﻿using batuhanbilet.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatuhanBilet.webui.Models
+{
+    public class BiletGuzergah
+    {
+        public List<batuhanbilet.entity.Bilet> Bilets { get; set; }
+        public List<Guzergah> Guzergahs { get; set; }
+        public List<Sehir> Sehirs { get; set; }
+        public Guzergah YeniGuzergah { get; set; }
+        public string saat { get; set; }
+        public string tarih { get; set; }
+        public Bilet bilet { get; set; }
+
+    }
+}
